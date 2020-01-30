7d9d477f6f733b8b017f3543ef3acbb8974d9453
francescoGalloni94 <francesco.galloni94@gmail.com>
2020-01-30 15:46:20 +0100

    fix setup.py using requirements file to install the right version

--- setup.py
+++ setup.py
@@ -2,6 +2,20 @@ from __future__ import absolute_import
 from setuptools import setup
 from setuptools import find_packages
 
+
+
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+
 setup(name='elephas',
       version='0.4.2',
       description='Deep learning on Spark with Keras',
@@ -9,7 +23,7 @@ setup(name='elephas',
       download_url='https://github.com/maxpumperla/elephas/tarball/0.4.2',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
-      install_requires=['cython', 'tensorflow', 'keras', 'hyperas', 'flask', 'six', 'pyspark'],
+      install_requires=load_requirements("requirements.txt"),
       extras_require={
         'java': ['pydl4j>=0.1.3'],
         'tests': ['pytest', 'pytest-pep8', 'pytest-cov', 'mock']
