9611c25649dcf4576b34364964594691a03ddb4b
francescoGalloni94 <francesco.galloni94@gmail.com>
2020-02-04 12:10:08 +0100

    fix transormer

--- elephas/transformers.py
+++ elephas/transformers.py
@@ -1,4 +1,6 @@
 from pyspark.sql import Row
+from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import GatewayParameters
 
 class Transformer(object):
     """Interface which defines a transformer object."""
@@ -12,11 +14,12 @@ class Transformer(object):
 
 class OntologyTransformer(Transformer):
 
-    def __init__(self, java_gateway_entry_point, input_col="prediction", context_col="context", output_col="refined_index"):
+    def __init__(self, java_gateway_address, java_gateway_port, input_col="prediction", context_col="context", output_col="refined_index"):
         self.input_column = input_col
         self.output_column = output_col
         self.context_col = context_col
-        self.java_gateway_entry_point = java_gateway_entry_point
+        self.java_gateway_address = java_gateway_address
+        self.java_gateway_port = java_gateway_port
 
     def transform(self, dataframe):
         return dataframe.rdd.map(self._transform).toDF()
@@ -28,7 +31,10 @@ class OntologyTransformer(Transformer):
         if context is None:
             index = float(self.get_index(prediction))
         else:
-            index = self.java_gateway_entry_point.refinePrediction(prediction.tolist(), 0, context)
+            parameters = GatewayParameters(address=self.java_gateway_address, port=self.java_gateway_port, auto_convert=True)
+            gateway = JavaGateway(gateway_parameters=parameters)
+            entry_point = gateway.entry_point
+            index = entry_point.refinePrediction(prediction.tolist(), 0, context)
             index = float(index)
         new_row = self.new_dataframe_row(row, self.output_column, index)
 
